e1b75ba8643c13c1acc0dbe30292a0e44807873e
IceCaptin <tjdqn0228@gmail.com>
2023-08-15 01:31:26 +0900

    SecurityConfig 수정

--- BusanProject/src/main/java/edu/pnu/config/SecurityConfig.java
+++ BusanProject/src/main/java/edu/pnu/config/SecurityConfig.java
@@ -13,40 +13,39 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 import edu.pnu.repository.BusanUserRepository;
 import lombok.RequiredArgsConstructor;
 
-
 @RequiredArgsConstructor
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
-	
+
 	private final AuthenticationConfiguration authConfig;
 	private final BusanUserRepository busanUserRepository;
-	
+
 	@Value("${jwt.secretKey}")
 	private String secretKey;
-	
+
+	@SuppressWarnings("removal")
 	@Bean
 	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 		http.csrf(csrf -> csrf.disable());
-		
+		http.cors();
+
 		http.authorizeHttpRequests(security -> security
-	               .requestMatchers("/comment/add").authenticated()
-	               .requestMatchers("/comment/**").permitAll()
-	               .requestMatchers("/admin/**").hasRole("ADMIN")
-	               .anyRequest().permitAll());
-		
+				.requestMatchers("/comment/add", "/comment/edit", "/comment/delete").authenticated()
+				.requestMatchers("/admin/**").hasRole("ADMIN")
+				.anyRequest().permitAll());
 
-//		http.authorizeHttpRequests(security -> security
-//			    .requestMatchers("/comment/**").permitAll() // 댓글 경로에 대해서는 누구나 접근 허용
-//			    .requestMatchers("/admin/**").hasRole("ADMIN") // 관리자 경로에 대해서는 ADMIN 역할이 있는 사용자만 접근 허용
-//			    .anyRequest().permitAll() // 나머지 모든 요청에 대해서는 누구나 접근 허용
-//			);
-
-
+		// http.authorizeHttpRequests(security -> security
+		// .requestMatchers("/comment/**").permitAll() // 댓글 경로에 대해서는 누구나 접근 허용
+		// .requestMatchers("/admin/**").hasRole("ADMIN") // 관리자 경로에 대해서는 ADMIN 역할이 있는
+		// 사용자만 접근 허용
+		// .anyRequest().permitAll() // 나머지 모든 요청에 대해서는 누구나 접근 허용
+		// );
 
 		http.formLogin(frmLogin -> frmLogin.disable()); // 폼 로그인 기반 비활성화
 		http.sessionManagement(ssmg -> ssmg.sessionCreationPolicy(SessionCreationPolicy.STATELESS)); // 세션 없애기. jwt 할거.
-		http.addFilterBefore(new JWTAuthorizationFilter(busanUserRepository, secretKey), UsernamePasswordAuthenticationFilter.class); // 권한검사
+		http.addFilterBefore(new JWTAuthorizationFilter(busanUserRepository, secretKey),
+				UsernamePasswordAuthenticationFilter.class); // 권한검사
 		return http.build();
 	}
 }
